6fa58d927380c966421168e799b5bfef4cf15f4c
amorim <gabrielmaia.amorim01@gmail.com>
2022-11-17 16:07:23 -0300

    First Commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,31 +1,31 @@
 import axios from 'axios'
 
-export const createPost = (dados) => axios.post("http://localhost:3001/homeFeed", dados)
+export const createPost = (dados) => axios.post("https://projeto-fullstack-react-node.herokuapp.com/homeFeed", dados)
 
-export const deletePost = (dados) => axios.delete("http://localhost:3001/homeFeed", {data: dados})
+export const deletePost = (dados) => axios.delete("https://projeto-fullstack-react-node.herokuapp.com/homeFeed", {data: dados})
 
-export const updatePost = (dados) => axios.patch("http://localhost:3001/homeFeed", dados)
+export const updatePost = (dados) => axios.patch("https://projeto-fullstack-react-node.herokuapp.com/homeFeed", dados)
 
 export const postUser = async (dados) => {
-    const res = await axios.post("http://localhost:3001/posts", dados)
+    const res = await axios.post("https://projeto-fullstack-react-node.herokuapp.com/posts", dados)
     const data = res.data
     return data
 }
 
 export const getAllPosts = async () => {
-    const res = await axios.get("http://localhost:3001/homeFeed")
+    const res = await axios.get("https://projeto-fullstack-react-node.herokuapp.com/homeFeed")
     const data = res.data
     return data
 }
 
 export const getUsername = async () => {
-    const res = await axios.get("http://localhost:3001/username")
+    const res = await axios.get("https://projeto-fullstack-react-node.herokuapp.com/username")
     const data = res.data
     return data
 }
 
 export const loginUser = async (dados) => {
-    const res = await axios.post("http://localhost:3001/login", dados)
+    const res = await axios.post("https://projeto-fullstack-react-node.herokuapp.com/login", dados)
     const data = res.data
     return data
 }
